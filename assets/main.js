ed6f28b22195e84ca54cdd49a8d56e4bd59344d2
emil.mekhtiyev <emil.mekhtiyev@outlook.com>
2022-05-30 13:49:37 +0500

    added function to count

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+// Устанавливаем дату до окончания отсчета
+const newYear = '1 Jan 2023';
+
+// Обращаемся к элементам
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
+
+function count(){
+    const newYearsDate = new Date(newYear);
+    const currentDate = new Date();
+    // Вычисляем общее количество секунд
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    // Вычисляем дни, часы, минуты и секунды
+    const Days = Math.floor(totalSeconds / 3600 / 24);
+    const Hours = Math.floor(totalSeconds / 3600) % 24;
+    const Mins = Math.floor(totalSeconds / 60) % 60;
+    const Secs = Math.floor(totalSeconds) % 60;
+
+    
+    // Подставляем зполученные значения в html
+    daysEl.innerHTML = Days;
+    hoursEl.innerHTML = formatTime(Hours);
+    minsEl.innerHTML = formatTime(Mins);
+    secsEl.innerHTML = formatTime(Secs);
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+count();
+setInterval(count, 1000);
